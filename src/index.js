ce7bb40fc48deed464d52ca5b1b3ff80d7f7e6de
Mattias Naarttijärvi <mattias.naarttijarvi@gmail.com>
2019-04-05 13:37:17 +0200

    Build

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import 'src/util/polyfill.js'
 import applyToNestedStrings from 'src/applyToNestedStrings.js'
 
 // Do dev-testing to your module here, just remeber to remove/comment it when builing for production
-window.applyToNestedStrings = applyToNestedStrings
+// window.applyToNestedStrings = applyToNestedStrings
 
 // Library export
 export default applyToNestedStrings
